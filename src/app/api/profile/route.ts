c66018b95cedb47164ec04b2df7be1183ef22483
ElkinDev <elkinmendoza00@gmail.com>
2025-05-20 18:56:19 +1000

    route header  block config

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,3 +1,19 @@
-export async function GET() {
-  return new Response("Profile API data");
+import { type NextRequest } from "next/server";
+import { headers } from "next/headers";
+export async function GET(request: NextRequest) {
+  // const requestHeaders = request.headers;
+  // console.log("Request Headers:", requestHeaders.get("Authorization"));
+  // console.log("Request URL:", requestHeaders);
+  const headerList = await headers();
+
+  console.log("Header List:", headerList.get("Authorization"));
+  console.log("Header List:", headerList.get("User-Agent"));
+  return new Response("<h1>Profile API data</h1>", {
+    status: 200,
+    headers: {
+      "Content-Type": "text/html",
+      Authorization: headerList.get("Authorization") || "",
+      "User-Agent": headerList.get("User-Agent") || "",
+    },
+  });
 }
