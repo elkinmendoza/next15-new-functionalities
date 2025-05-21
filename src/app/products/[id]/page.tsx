7cb856fddbf8ebecf54d35bf88ebce5c2f1a2033
ElkinDev <elkinmendoza00@gmail.com>
2025-05-22 00:48:31 +1000

    Diving into dynamicParams, to control what happens when a dynamic segment is visited that was not generated with generateStaticParams() Next.js will statically render pages on demand for any dynamic segments not included  in generateStaticParams ext.js will return 404 error for any dynamic segments not included in our pre-renderered list

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamicParams = false;
+
 export async function generateStaticParams() {
   const categories = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }];
   return categories.map((category) => ({
