d3c57ebc619c3325a8378e8a9ba190be0198f03c
ElkinDev <elkinmendoza00@gmail.com>
2025-05-23 00:32:47 +1000

    using client-only code npm i client-only to allow using client base component in only RCC

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -2,16 +2,18 @@
 import React from "react";
 
 import { useTheme } from "@/components/theme-provider";
+import { ClientSideFunction } from "@/utils/client-utils";
 
 export default function ClientRoutePage() {
     const theme = useTheme();
+    const result = ClientSideFunction();
 
   return (
     <section className="w-full h-screen  my-12 m-auto">
       <div className="image-slider-container">
          <h1 style={{color:theme.colors.primary}}> Client Route</h1>
          <h1 style={{color:theme.colors.secondary}}> Secondary Route</h1>
-
+         <p>{result}</p>
       </div>
     </section>
   );
--- src/app/server-route/page.tsx
+++ src/app/server-route/page.tsx
@@ -1,11 +1,13 @@
 import { serverSideFunction } from "@/utils/server-utils";
 import { ImageSlider } from "@/components/ImageSlider";
-
+// import { ClientSideFunction } from "@/utils/client-utils";
 export default function ServerRoutePage() {
     const result = serverSideFunction();
+    // const clientResult = ClientSideFunction();
     return (
         <section className="w-full h-screen  my-12 m-auto">
         <h1>Server Route {result}</h1>
+        {/* <h1>Client Route {clientResult}</h1> */}
 
         <ImageSlider />
         </section>
